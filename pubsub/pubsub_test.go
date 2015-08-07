4a83615bd65f92026fa4e80b985b4b8d7200380f
mewing <anamewing@gmail.com>
2015-08-08 00:34:42 +0800

    complete subpub_test

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -1,24 +1,68 @@
 package pubsub
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestPubsub(t *testing.T) {
 	//	compile time assertion
-	//	var _ pubsub.Publisher = (*pubsub.Pubsub)(nil)
-	//	run time check, both is ok
-	var p interface{} = &Pubsub{}
-	if _, ok := p.(MessageQueue); !ok {
-		t.Fatal("Pubsub does not implement MessageQueue")
+	Publisher := NewPubsub()
+	eventTypes := []string{"notes", "images", "videos"}
+	suberPerType := 20
+	suberList := make([]*Suber, 0, len(eventTypes)*suberPerType)
+
+	//	make subscribers
+	for _, eventType := range eventTypes {
+		for i := 0; i < suberPerType; i++ {
+			sub := &Suber{tester: t, eventT: eventType, eventCounter: make(map[string]int)}
+			suberList = append(suberList, sub)
+			Publisher.Subscribe(eventType, sub)
+		}
 	}
 
+	//	start publish
+	eventMap := make(map[string](map[string]int))
+	for _, eventType := range eventTypes {
+		eventMap[eventType] = make(map[string]int)
+	}
+
+	pubStart := time.Now()
+	for i := 0; i < len(eventTypes); i++ {
+		for j := 0; j < 2; j++ {
+			pubTimes := rand.Intn(30)
+			eventName := fmt.Sprintf("Name%d", j)
+			eventMap[eventTypes[i]][eventName] += pubTimes
+			for k := 0; k < pubTimes; k++ {
+				Publisher.Publish(eventTypes[i], eventName)
+			}
+
+		}
+	}
+	t.Logf("pub runs %v", time.Now().Sub(pubStart))
+
+	//	check subers
+	for _, suber := range suberList {
+		for eventName, counts := range eventMap[suber.eventT] {
+			// t.Logf("pub %s on %s for %d times", suber.eventT, eventName, counts)
+			if suber.eventCounter[eventName] != counts {
+				t.Errorf("suber  count event %s: %d, expect %d", eventName, suber.eventCounter[eventName], counts)
+			}
+		}
+	}
+}
+
 type Suber struct {
-	tester *testing.T
-	eventT string
+	tester       *testing.T
+	eventT       string
+	eventCounter map[string]int
 }
 
 func (s *Suber) Notify(event Event) error {
-	s.tester.Logf(event.(string))
+	// s.tester.Logf("suber %p listen on %s get %s", s, s.eventT, event.(string))
+	s.eventCounter[event.(string)]++
+	time.Sleep(10 * time.Millisecond)
 	return nil
 }
