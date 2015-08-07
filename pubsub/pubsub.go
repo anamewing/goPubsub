e962fd69c9154c4e6c97c2baadf60d818fb66e51
mewing <anamewing@gmail.com>
2015-08-07 16:05:03 +0800

    implement MessageQueue in pubsub.Pubsub

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,6 +1,7 @@
 package pubsub
 
 type Pubsub struct {
+	subscriberMap map[string]([]Subscriber)
 }
 
 type Event interface{}
@@ -12,4 +13,16 @@ type MessageQueue interface {
 
 type Subscriber interface {
 	Notify(event Event) error
+}
+
+func (p *Pubsub) Publish(eventType string, event Event) error {
+	for _, subscriber := range p.subscriberMap[eventType] {
+		subscriber.Notify(event)
+	}
+	return nil
+}
+
+func (p *Pubsub) Subscribe(eventType string, subscriber Subscriber) error {
+	p.subscriberMap[eventType] = append(p.subscriberMap[eventType], subscriber)
+	return nil
 }
