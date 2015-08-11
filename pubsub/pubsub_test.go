a87fd2d160a62e761423e3578e57fb89eef37c7f
mewing <anamewing@gmail.com>
2015-08-11 20:16:17 +0800

    test with blocking notify

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -14,10 +14,16 @@ func TestPubsub(t *testing.T) {
 	suberPerType := 20
 	suberList := make([]*Suber, 0, len(eventTypes)*suberPerType)
 
+	eventNameCount := 20
+	eventMaxPubTimes := 30
+	chanMaxCount := len(eventTypes) * eventNameCount * eventMaxPubTimes * suberPerType
+
+	bufChan := make(chan bool, chanMaxCount)
+
 	//	make subscribers
 	for _, eventType := range eventTypes {
 		for i := 0; i < suberPerType; i++ {
-			sub := &Suber{tester: t, eventT: eventType, eventCounter: make(map[string]int)}
+			sub := &Suber{tester: t, eventT: eventType, eventCounter: make(map[string]int), bufChan: bufChan}
 			suberList = append(suberList, sub)
 			Publisher.Subscribe(eventType, sub)
 		}
@@ -29,6 +35,7 @@ func TestPubsub(t *testing.T) {
 		eventMap[eventType] = make(map[string]int)
 	}
 
+	realPubTimes := 0
 	pubStart := time.Now()
 	for i := 0; i < len(eventTypes); i++ {
 		for j := 0; j < 2; j++ {
@@ -38,17 +45,21 @@ func TestPubsub(t *testing.T) {
 			for k := 0; k < pubTimes; k++ {
 				Publisher.Publish(eventTypes[i], eventName)
 			}
-
+			realPubTimes += pubTimes
 		}
 	}
 	t.Logf("pub runs %v", time.Now().Sub(pubStart))
 
+	//	blocking
+	for i := 0; i < realPubTimes*suberPerType; i++ {
+		<-bufChan
+	}
 	//	check subers
 	for _, suber := range suberList {
 		for eventName, counts := range eventMap[suber.eventT] {
 			// t.Logf("pub %s on %s for %d times", suber.eventT, eventName, counts)
 			if suber.eventCounter[eventName] != counts {
-				t.Errorf("suber  count event %s: %d, expect %d", eventName, suber.eventCounter[eventName], counts)
+				t.Errorf("suber %p count %s event %s: %d, expect %d", suber, suber.eventT, eventName, suber.eventCounter[eventName], counts)
 			}
 		}
 	}
@@ -58,11 +69,13 @@ type Suber struct {
 	tester       *testing.T
 	eventT       string
 	eventCounter map[string]int
+	bufChan      chan bool
 }
 
 func (s *Suber) Notify(event Event) error {
 	// s.tester.Logf("suber %p listen on %s get %s", s, s.eventT, event.(string))
 	s.eventCounter[event.(string)]++
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
+	s.bufChan <- true
 	return nil
 }
